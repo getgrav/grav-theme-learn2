2e74ac1e7615edf8a77448896b3fee195e98b85b
Djamil Legato <djamil@djamil.it>
2015-10-06 09:31:54 -0700

    Enhanced code and pre blocks with 1-click clipboard icon

--- js/learn.js
+++ js/learn.js
@@ -135,6 +135,35 @@ jQuery(document).ready(function(){
     jQuery('[data-search-input]').trigger('input');
   }
 
+  // clipboard
+	var clipInit = false;
+	$('code').each(function(){
+		var code = $(this),
+			text = code.text();
+
+		if (text.length > 5) {
+      if (!clipInit){
+  			var text, clip = new Clipboard('.copy-to-clipboard', {
+  				text: function(trigger) {
+  					text = $(trigger).prev('code').text();
+  	        return text.replace(/^\$\s/gm, '');
+  		    }
+  			});
+
+  			var inPre;
+  			clip.on('success', function(e) {
+  				inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
+  				$(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
+  			});
+  		}
+
+  		code.after('<span class="copy-to-clipboard" title="Copy to clipboard" />');
+  		code.next('.copy-to-clipboard').on('mouseleave', function(){
+  			$(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
+  		});
+    }
+	});
+
 });
 
 jQuery(window).on('load',function(){
